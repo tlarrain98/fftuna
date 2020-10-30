301b54974f0d178f6b4db81bfd9e1689b8606850
Tomas Larrain <tlarrain@wisc.edu>
2020-10-30 13:35:09 -0500

    refactored againg

--- client/src/components/landingpages/Landing.js
+++ client/src/components/landingpages/Landing.js
@@ -1,46 +1,39 @@
 import React, { useState } from 'react';
 import LoginButton from './LoginButton.js';
 import '../../css/Landing.css';
-// import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 import SignUpButton from './SignUpButton.js';
-import LoginWindow from './LoginWindow.js';
-import RegisterWindow from './RegisterWindow.js';
+// import LoginWindow from './LoginWindow.js';
+// import RegisterWindow from './RegisterWindow.js';
 
 const Landing = (props) => {
 
     const [state, setState] = useState('landing');
     
-    // const isAuthenticated = useAuth0().isAuthenticated;
+    const isAuthenticated = useAuth0().isAuthenticated;
+    const { isLoading } = useAuth0();
 
-    // if(isAuthenticated) {
-    //     props.goHome();
-    // }
-
-    const chooseDisplay = () => {
-        if(state === 'landing') {
-            return(
-                <div className="landingMargin">
-                    <text className="title">Show Me Your Tuna</text>
-                    <SignUpButton setState={setState}/>
-                    <LoginButton setState={setState}/>
-                </div>
-            )
-        }
-        if(state === 'login') {
-            return(
-                <LoginWindow setState={setState}/>
-            )
-        }
-        if(state === 'register') {
-            return(
-                <RegisterWindow setState={setState}/>
-            )
-        }
+    if(isAuthenticated) {
+        props.goHome();
     }
 
-    return(
+    if(isLoading) {
+        return(
+            <div className="landing">
+                <div className="landingMargin">
+                    <text className="loading">Loading...</text>
+                </div>
+            </div>
+        )
+    }
+
+    return (
         <div className="landing">
-            {chooseDisplay()}
+            <div className="landingMargin">
+                <text className="title">Show Me Your Tuna</text>
+                <SignUpButton />
+                <LoginButton />
+            </div>
         </div>
     )
 }
--- client/src/components/landingpages/LoginButton.js
+++ client/src/components/landingpages/LoginButton.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import '../../css/Landing.css';
-// import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 
-const LoginButton = (props) => {
-    // const { loginWithRedirect } = useAuth0();
+const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
 
     return(
         <div>
             <Button className="loginButton"
                 size="lg"
-                onClick={() => props.setState('login')}>
+                onClick={() => loginWithRedirect()}>
                 Log In
             </Button>
         </div>
