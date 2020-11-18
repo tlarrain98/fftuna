a545d7c81313436ef3277f291c147b752854f1be
Tomas Larrain <tlarrain@wisc.edu>
2020-11-18 17:29:12 -0600

    server update

--- server/main/app.js
+++ server/main/app.js
@@ -4,13 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes.js')
+
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use('/', indexRouter)
 
 module.exports = app;
