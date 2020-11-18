71b6efe1866d8e12f4cf69c00edfd74c7b347e42
Tomas Larrain <tlarrain@wisc.edu>
2020-11-18 12:46:00 -0600

    adding build

---
+++ client/build/precache-manifest.30c4555076455b6d952ba19d5880d709.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315012052a34b864cdcefe017979a148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf182f74a4bd35eb5d4",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "878c435e1db1c0a9d418",
+    "url": "/static/css/main.077ad817.chunk.css"
+  },
+  {
+    "revision": "2cf182f74a4bd35eb5d4",
+    "url": "/static/js/2.5b4fb3cc.chunk.js"
+  },
+  {
+    "revision": "bfe7376331c3338b61ed6a4a38b63dd9",
+    "url": "/static/js/2.5b4fb3cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878c435e1db1c0a9d418",
+    "url": "/static/js/main.619c6525.chunk.js"
+  },
+  {
+    "revision": "ba9c8fbb9f725fe83f79",
+    "url": "/static/js/runtime-main.4e92a8be.js"
+  },
+  {
+    "revision": "2e9cebd1e9a39bffc7b3b1c66fffe89f",
+    "url": "/static/media/hoffvote.2e9cebd1.png"
+  },
+  {
+    "revision": "bd801e6c9ace2b897b0ae557f5dbb7fa",
+    "url": "/static/media/medovote.bd801e6c.png"
+  },
+  {
+    "revision": "0606e4546fe792ef4fc183a6766bd413",
+    "url": "/static/media/zucc.0606e454.png"
+  }
+]);
